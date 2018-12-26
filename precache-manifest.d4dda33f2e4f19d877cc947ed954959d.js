81dd378524d27e89d6edb9021aa2f99902b2d835
Nk <nazmulkabir512@gmail.com>
2018-12-27 02:09:17 +0600

    Updates

---
+++ precache-manifest.d4dda33f2e4f19d877cc947ed954959d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "86affca87c22be9c51b4",
+    "url": "/food-rating-app/static/js/runtime~main.86affca8.js"
+  },
+  {
+    "revision": "84af719593d2791d613d",
+    "url": "/food-rating-app/static/js/main.84af7195.chunk.js"
+  },
+  {
+    "revision": "ecb7acda53639972227c",
+    "url": "/food-rating-app/static/js/1.ecb7acda.chunk.js"
+  },
+  {
+    "revision": "84af719593d2791d613d",
+    "url": "/food-rating-app/static/css/main.a0318969.chunk.css"
+  },
+  {
+    "revision": "0e5c7e6e2a0006639062c5386783b25a",
+    "url": "/food-rating-app/index.html"
+  }
+];
